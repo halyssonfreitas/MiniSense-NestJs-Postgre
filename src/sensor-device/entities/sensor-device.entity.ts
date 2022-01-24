0dc95df589b199301585fa50256d347c650fd485
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-24 14:30:44 -0300

    #exec npm install class-transformer

--- src/sensor-device/entities/sensor-device.entity.ts
+++ src/sensor-device/entities/sensor-device.entity.ts
@@ -1,1 +1,19 @@
-export class SensorDevice {}
+import { IsEmpty, IsString } from "class-validator";
+import { Column, PrimaryColumn } from "typeorm";
+
+export class SensorDevice {
+
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    @IsString()
+    @IsEmpty()
+    key?: string;
+
+    label: string;
+
+    description: string;
+
+    user: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,11 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
-import {IsEmail} from "class-validator";
+import {IsEmail, IsEmpty} from "class-validator";
 
 @Entity()
 export class User {
 
     @PrimaryColumn()
+    @IsEmpty()
     id: string;
 
     @Column()
