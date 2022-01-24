1e0a78f955174f6cc192083abf59d30a7ddfa8d8
Halysson Freitas <halyssonfreitas@gmail.com>
2022-01-24 13:29:26 -0300

    #update User entity and yours DTOs

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    username: string;
+    @IsEmail()
+    email: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import {IsEmail} from "class-validator";
+
+@Entity()
+export class User {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    @IsEmail()
+    email: string;
+}
